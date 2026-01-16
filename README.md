974bb83c375667d49c4ba25b88adce368f88c741
José David <jcabmar2012@g.educaand.es>
2026-01-16 12:28:51 +0100

    Editado Manifest

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # servicio_mantenimiento
+El siguiente
+proyecto tiene como objetivo desarrollar un nuevo módulo para odoo que gestione el
+servicio de mantenimiento de los productos con garantía vendidos por una empresa
---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# servicio_manteminiento/__manifest__.py
+{
+    'name': 'Servicio de Mantenimiento',
+    'version': '1.0',
+    'author': 'Paco Navas',
+    'category': 'Custom',
+    'summary': 'Gestión de Mantenimiento de Productos',
+    'depends': ['base', 'garantia.producto'],
+    'data': [
+        'views/servicio_mantenimiento_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'icon': '/garantias/static/description/icon55.png',
+    'installable': True,
+    'application': True,    
+}
